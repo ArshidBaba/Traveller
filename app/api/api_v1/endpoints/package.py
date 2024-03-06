b370a003a57c8641601158a3ef9a2e23efed4a62
Arshid <arshidbaba93@gmail.com>
2024-03-07 00:53:32 +0530

    Added GET route to packages API

--- app/api/api_v1/endpoints/package.py
+++ app/api/api_v1/endpoints/package.py
@@ -47,18 +47,6 @@ def update_package(
     return package
 
 
-# @router.get("/", status_code=200, response_model=LocationSearchResults)
-# def fetch_locations(
-#     *, max_results: Optional[int] = 10, db: Session = Depends(deps.get_db)
-# ) -> dict:
-#     """
-#     Get all locations
-#     """
-#     locations = crud.location.get_multi(db=db, limit=max_results)
-
-#     return {"results": locations}
-
-
 @router.get("/", status_code=200, response_model=PackageSearchResults)
 def fetch_packages(
     *, max_results: Optional[int] = 10, db: Session = Depends(deps.get_db)
@@ -69,3 +57,35 @@ def fetch_packages(
     packages = crud.package.get_multi(db=db, limit=max_results)
 
     return {"results": packages}
+
+
+# @router.get("/{location_id}", status_code=200, response_model=Location)
+# def fetch_location(
+#     *,
+#     location_id: int,
+#     db: Session = Depends(deps.get_db),
+# ) -> Any:
+#     """
+#     Fetch a single recipe by ID
+#     """
+#     result = crud.location.get(db=db, id=location_id)
+#     if not result:
+#         # the exception is raised, not returned - you will get a validation
+#         # error otherwise.
+#         raise HTTPException(
+#             status_code=404, detail=f"Recipe with ID {location_id} not found"
+#         )
+
+
+#     return result
+@router.get("/{package_id}", status_code=200, response_model=Package)
+def fetch_package(*, package_id: int, db: Session = Depends(deps.get_db)) -> Any:
+    """
+    Fetch a single Package by ID.
+    """
+    result = crud.package.get(db=db, id=package_id)
+    if not result:
+        raise HTTPException(
+            status_code=404, detail=f"Package with ID {package_id} not found"
+        )
+    return result
